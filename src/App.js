ec4d01a2b5bb4ef4b21276d92d87758e5046b06e
Willian Ricardo <willianricardoleal@gmail.com>
2022-08-24 22:41:43 -0300

    Components

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./App.css"
+import ComponenteUm from "./components/ComponenteUm"
+function App(){
+    return (
+        <>
+            <ComponenteUm/>
+        </>
+    )
+}
+export default App
---
+++ src/components/ComponenteUm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ComponenteUm() {
+    return (
+        <div className="componente">
+            <p>Titulo: Componente um</p>
+            <p>Componente mais simples de React</p>
+        </div>
+    )
+}
+export default ComponenteUm
